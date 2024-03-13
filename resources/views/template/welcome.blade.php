a2f2bada40369acc9be03551afa4c67e92c15652
littleBlackMan <sandyrazafitrimo@gmail.com>
2024-03-13 22:26:18 +0100

    fix scss and add sass

--- resources/views/template/welcome.blade.php
+++ resources/views/template/welcome.blade.php
@@ -1,1 +1,11 @@
-Hello welcome
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
+</head>
+<body>
+<h1>Hello<span>World</span></h1>
+</body>
+</html>
