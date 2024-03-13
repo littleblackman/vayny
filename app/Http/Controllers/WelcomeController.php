81fff4112961953bff31d00af4669a030e006939
littleBlackMan <sandyrazafitrimo@gmail.com>
2024-03-13 17:57:02 +0100

    add the welcome controller

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\View\View;
+
+class WelcomeController extends Controller
+{
+    /**
+     * Show the welcome page.
+     *
+     * @return View
+     */
+    public function index(): View
+    {
+        return view('template/welcome');
+    }
+}
